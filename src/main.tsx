cbf1dde5078bdd499eee393c4939e44e30cf7597
unxok <anderluke707@gmail.com>
2024-05-20 21:40:50 -0700

    idk why I cant get this to work

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+import "./index.css";
+import { Notice, Plugin } from "obsidian";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default class samplePlugin extends Plugin {
+	onload(): void {
+		new Notice("Sample plugin loaded");
+	}
+
+	unload(): void {
+		new Notice("Sample plugin disabled");
+	}
+
+	// renderRoot() {
+	// 	ReactDOM.createRoot(document.getElementById("root")!).render(
+	// 		<React.StrictMode>
+	// 			<App />
+	// 		</React.StrictMode>
+	// 	);
+	// }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: "manifest.json",
+					dest: "./",
+				},
+			],
+		}),
+	],
+	build: {
+		outDir: "testing-vault/.obsidian/plugins/obsidian-vite-react",
+		assetsDir: "./",
+		rollupOptions: {
+			external: ["obsidian"],
+			input: "src/main.tsx",
+			output: {
+				format: "cjs",
+				exports: "default",
+				entryFileNames: "main.js",
+				assetFileNames: "styles.css",
+			},
+			plugins: [
+				commonjs(),
+				resolve({
+					browser: true,
+				}),
+			],
+		},
+	},
+});
